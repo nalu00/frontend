ac27391e402612cac51f5eb4384879e7d89efcec
Ana Luísa <a.oliveira4@iesb.edu.br>
2026-04-09 21:52:22 -0300

    atualizando aula03

--- aulas/aula03/src/pages/Boletos.jsx
+++ aulas/aula03/src/pages/Boletos.jsx
@@ -1,15 +1,26 @@
-import Sidebar from "../components/Sidebar";
-import Topbar from "../components/Topbar";
 import Layout from "./Layout";
+import Tabela from "../components/Tabela";
 
 function Boletos() {
-    return <>
-        <Layout>
-            <h2>Meus Boletos</h2>
-            <section>
-            </section>
-        </Layout>
-    </>
+    const colunas = [
+        "Vencimento",
+        "Valor",
+        "Situação"
+    ];
+
+    const boletos = [
+        {Vencimento: "01/04/2026", valor:500, situação: "Pago"},
+        {Vencimento: "01/05/2026", valor:500, situação: "A vencer"},
+        {Vencimento: "01/06/2026", valor:500, situação: "A vencer"},
+        {Vencimento: "01/07/2026", valor:500, situação: "A vencer"},
+        {Vencimento: "01/08/2026", valor:500, situação: "A vencer"},
+    ];
+
+  return (
+    <Layout titulo="Meus Boletos" subtitulo="Histórico de Pagamentos">
+      <Tabela titulos={colunas} dados={boletos}/>
+    </Layout>
+  );
 }
 
 export default Boletos;
--- aulas/aula03/src/pages/Requerimentos.jsx
+++ aulas/aula03/src/pages/Requerimentos.jsx
@@ -1,13 +1,13 @@
 import Layout from "./Layout";
+import Tabela from "../components/Tabela";
 
 function Requerimentos() {
-    return <>
-        <Layout>
-            <h2>Meus Requerimentos</h2>
-            <section>
-            </section>
-        </ Layout>
-    </>
+  return (
+    <Layout
+      titulo="Meus Requerimento" subtitulo="Faça solicitações online para a secretaria">
+      <Tabela />
+    </Layout>
+  );
 }
 
 export default Requerimentos;
